9f350c472d0635ccd5492e34c61aac7308984ebf
AGhost-7 <jonathan.boudreau.92@gmail.com>
2020-03-11 16:56:21 -0400

    remove unused configurations

--- guake/dump.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-gconftool-2 --dump /schemas/apps/guake > schemas.xml
-gconftool-2 --dump /apps/guake > apps.xml
--- guake/load.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-gconftool-2 --load apps.xml
-gconftool-2 --load schemas.xml
