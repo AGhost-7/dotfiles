27bf9c09dac983d9d5997be564026831ce067f0b
AGhost-7 <jonathan-boudreau@protonmail.com>
2024-07-24 23:02:31 -0400

    add new lua config

---
+++ init.lua
@@ -0,0 +1,86 @@
+-- {{{ Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+-- }}}
+
+vim.opt.foldmethod = "marker"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- {{{ configure plugins
+require("lazy").setup({
+  spec = {
+    -- color theme
+    {
+        "morhetz/gruvbox",
+        config = function()
+            vim.cmd([[colorscheme gruvbox]])
+        end,
+        priority = 1000,
+    },
+    {'nvim-tree/nvim-web-devicons'},
+    -- bottom status line
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = "nvim-tree/nvim-web-devicons",
+    },
+    -- top status line
+    {
+        "akinsho/bufferline.nvim",
+        dependencies = "nvim-tree/nvim-web-devicons",
+    },
+    -- dims code
+    {"junegunn/limelight.vim"},
+    -- minimalist mode
+    {'junegunn/goyo.vim'},
+    -- file tree browser
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = "nvim-tree/nvim-web-devicons",
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        end,
+    },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  -- install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+--- }}}
+require('lualine').setup({
+    options = { theme = "gruvbox" }
+})
+require('bufferline').setup({})
+require("nvim-tree").setup({})
+
+vim.cmd('abbreviate t NvimTreeOpen')
+--vim.api.nvim_create_user_command('T', 'echo "It works!"', {})
